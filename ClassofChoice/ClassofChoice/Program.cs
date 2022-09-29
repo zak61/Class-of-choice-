b556ba022f02df8138d77f61654cf98ed6be02bb
zakariya mohamed <zakariyam614@gmail.com>
2022-09-29 11:11:08 -0400

    ClassOfChoice

---
+++ ClassofChoice/ClassofChoice/Program.cs
@@ -0,0 +1,93 @@
+﻿Console.WriteLine("Welcome to a quiz on geography!");
+Console.WriteLine("Question 1");
+Console.WriteLine("what is the capital of russia?");
+var moscow = Console.ReadLine();
+
+
+{
+
+
+    if (moscow == "moscow")
+    {
+        
+        Console.WriteLine("you got the answer correct");
+
+    }
+    else
+    {
+        Console.WriteLine("Wrong");
+    }
+
+
+    Console.WriteLine(" question 2");
+    Console.WriteLine(" What is the capital of somalia?");
+    var mogdishu = Console.ReadLine();
+    if (mogdishu == "mogdishu")
+    {
+        Console.WriteLine("you got the correct answer?");
+    }
+    else
+    {
+        Console.WriteLine("Wrong");
+    }
+
+    Console.WriteLine("question 3");
+    Console.WriteLine("What is the capital of france?");
+    var paris = Console.ReadLine();
+    if (paris == "paris")
+    {
+        Console.WriteLine("you got  the correct answer");
+      
+    }
+    else
+    {
+        Console.WriteLine("Wrong");
+    }
+
+
+    Console.WriteLine("question 4");
+    Console.WriteLine("In which country is mount everest located?");
+    var nepal = Console.ReadLine();
+    if (nepal == "nepal")
+    {
+        Console.WriteLine("you got  the correct answer");
+    }
+    else
+    {
+        Console.WriteLine("Wrong");
+    }
+
+
+
+    Console.WriteLine("question 6");
+    Console.WriteLine("In which country is the burj khalifa");
+    var uae = Console.ReadLine();
+    if (uae == "uae")
+    {
+        Console.WriteLine("you got  the correct answer");
+  
+
+    }
+    else
+    {
+        Console.WriteLine("Wrong");
+    }
+
+
+    Console.WriteLine("question 7");
+    Console.WriteLine("In which country is mount kilimanjaro");
+    var tanzania = Console.ReadLine();
+    if (tanzania == "tanzania")
+    {
+        Console.WriteLine("you got  the correct answer");
+
+
+    }
+    else
+    {
+        Console.WriteLine("Wrong");
+    }
+
+
+
+}
